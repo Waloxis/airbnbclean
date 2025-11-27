8fd828cfb8c5082f0eb9118dc2aa128c1d8840e7
Walid Amsaf <walidamsaf@Walids-MacBook-Air.local>
2025-11-27 13:15:30 -0500

    Disable DB/JPA auto-config for Render

--- src/main/java/com/example/airbnbclean/AirbnbcleanApplication.java
+++ src/main/java/com/example/airbnbclean/AirbnbcleanApplication.java
@@ -2,10 +2,17 @@ package com.example.airbnbclean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
 
-@SpringBootApplication
 @RestController
 @RequestMapping("/api")
 public class AirbnbcleanApplication {
